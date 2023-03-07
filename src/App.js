0ebb356fbfad4cdd1517ab1ec1cd25ae4cf10fea
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-07 12:51:38 +0900

    NavLink로 변경, NoPath 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Route, Routes} from "react-router";
 import Home from "./pages/Home";
 import Stuff from "./pages/Stuff";
 import Contact from "./pages/Contact";
+import {NavLink} from "react-router-dom";
+import NoPath from "./pages/NoPath";
 
 // localhost:3000           -> Home
 // localhost:3000/stuff     -> Stuff
@@ -13,15 +15,16 @@ function App() {
     <div>
       <h1>Simple SPA</h1>
       <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/stuff">Stuff</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/stuff">Stuff</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
       <div className="content">
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/stuff' element={<Stuff />} />
               <Route path='/contact' element={<Contact />} />
+              <Route path='*' element={<NoPath />} />
           </Routes>
       </div>
     </div>
---
+++ src/pages/NoPath.js
@@ -0,0 +1,11 @@
+import React from "react";
+const NoPath = () => {
+    return (
+        <div>
+            <h2>No Path</h2>
+            <p>404 - No Resource!!</p>
+        </div>
+    );
+}
+
+export default NoPath;
